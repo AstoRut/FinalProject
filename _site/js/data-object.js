1be371d07969fd1c11620dd35bafc9d329068a3c
AstoRut <viberate@aol.com>
2023-03-03 10:35:53 -0500

    removed _site from .gitignore

---
+++ _site/js/data-object.js
@@ -0,0 +1,19 @@
+region = {
+  Americas:
+    '<ul><li><a href="/pages/chicago/">Chicago</a><li><a href="/pages/costa_rica/">Costa Rica</a><li><a href="/pages/maui/">Maui</a>',
+  Europe:
+    '<ul><li><a href="/pages/iceland/">Iceland</a><li><a href="/pages/rome/">Rome</a><li><a href="/pages/vienna/">Vienna</a>',
+  Asia: '<ul><li><a href="/pages/indonesia/">Indonesia</a><li><a href="/pages/japan/">Japan</a><li><a href="/pages/taiwan/">Taiwan</a>',
+};
+
+destinations = [
+  "Chicago",
+  "Costa Rica",
+  "Maui",
+  "Iceland",
+  "Rome",
+  "Vienna",
+  "Indonesia",
+  "Japan",
+  "Taiwan",
+];
